5e352ef456b22270781d6c619294f4d4b4808689
jayceeth <75772603+jayceeth@users.noreply.github.com>
2021-03-19 22:36:58 -0700

    Initial commit

---
+++ src/SearchEngine.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./SearchEngine.css";
+export default function SearchEngine() {
+  return (
+    <div className="SearchEngine">
+      <form id="search-form" className="mb-3">
+        <div className="row">
+          <div className="col-4">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              id="city-input"
+              autocomplete="off"
+            />
+          </div>
+          <div class="col-2">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+              id="submit-button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
